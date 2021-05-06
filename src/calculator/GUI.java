5eed07602f12d643a155b8d127c9f126326a96e4
QrxLP <maxsss@outlook.de>
2021-05-06 14:36:28 +0200

    GUI läuft

--- src/calculator/GUI.java
+++ src/calculator/GUI.java
@@ -1,19 +1,22 @@
 package calculator;
 import javax.swing.*;
-import  java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class GUI extends JFrame {
 
 
     private JPanel rootPanel;
-    private JButton button1;
+    private JButton enter;
     private JTextField input1;
     private JTextField input2;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private int operaterNr;
+    private JButton add;
+    private JButton sub;
+    private JButton mult;
+    private JButton diff;
+    private JLabel operator;
+    private int operatorNr;
 
     public GUI()
     {
@@ -21,7 +24,49 @@ public class GUI extends JFrame {
         add(rootPanel);
         setSize(400, 400);
 
+        enter.setBorderPainted(false);
+        enter.setMnemonic(KeyEvent.VK_ENTER);
+        add.setBorderPainted(false);
+        add.setMnemonic(KeyEvent.VK_ADD);
+        sub.setBorderPainted(false);
+        sub.setMnemonic(KeyEvent.VK_SUBTRACT);
+        mult.setBorderPainted(false);
+        mult.setMnemonic(KeyEvent.VK_MULTIPLY);
+        diff.setBorderPainted(false);
+        diff.setMnemonic(KeyEvent.VK_DIVIDE);
 
+
+
+
+
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operatorNr= 0;
+                operator.setText("+");
+            }
+        });
+        sub.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operatorNr = 1;
+                operator.setText("-");
+            }
+        });
+        mult.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operatorNr = 2;
+                operator.setText("*");
+            }
+        });
+        diff.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operatorNr = 3;
+                operator.setText("/");
+            }
+        });
     }
 
 }
