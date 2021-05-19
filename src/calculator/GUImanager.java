1c9d60bf74296b6ca9d4dc2aaa1888b38643a644
QrxLP <maxsss@outlook.de>
2021-05-19 10:27:36 +0200

    gui struktur

---
+++ src/calculator/GUImanager.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class GUImanager {
+
+    GUIelementar  guiElementar;
+
+    public  static void main (String[] args){
+
+        new GUImanager();
+
+    }
+
+    GUImanager(){
+        guiElementar = new GUIelementar();
+
+    }
+}
