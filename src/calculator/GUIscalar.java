2006dcca382a696ec238315792f1082174b7a8b3
Hldegrd <Sp.Lukas@outlook.com>
2021-05-20 10:19:33 +0200

    includes GUIscalar

--- src/calculator/GUIscalar.java
+++ src/calculator/GUIscalar.java
@@ -5,9 +5,14 @@ import javax.swing.*;
 
 public class GUIscalar extends JFrame {
     private JPanel rootPanel;
+    private JTextField scalaroperator;
 
     public void m_setvisible(boolean visible) {
         setVisible(visible);
+    }
+
+    public static void main(String [] args) {
+
     }
 
 }
