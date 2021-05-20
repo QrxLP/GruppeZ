9b9e332a2684ba86ca3fef0f8d69cd7b4b644a23
QrxLP <maxsss@outlook.de>
2021-05-20 11:08:54 +0200

    as

--- src/calculator/GUIscalar.java
+++ src/calculator/GUIscalar.java
@@ -4,7 +4,20 @@ import javax.swing.*;
 
 public class GUIscalar extends JFrame {
 
+    private JPanel rootPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
 
+
+    GUIscalar(){
+        add(rootPanel);
+        setSize(400, 400 );
+        setLocationRelativeTo(null);
+    }
 
 }
 
