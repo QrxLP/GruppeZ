accf87c9268ae8a2350f37e75e21b6888209ce3b
BavarianLP <Michael.Zeilmaier@t-online.de>
2021-05-20 10:19:19 +0200

    GUIpythagoras continued

--- src/calculator/GUIpythagoras.java
+++ src/calculator/GUIpythagoras.java
@@ -2,6 +2,23 @@ package calculator;
 
 import javax.swing.*;
 
-public class GUIpythagoras extends JFrame{
+public class GUIpythagoras extends JFrame {
     private JPanel rootPanel;
+    private JTextPane katheteATextPane;
+    private JTextPane textPane2;
+    private JTextPane textPane3;
+
+    public static void main(String[] args) {
+        new GUIpythagoras();
+    }
+
+    public GUIpythagoras() {
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        add(rootPanel);
+        setSize(400,400);
+    }
+
 }
