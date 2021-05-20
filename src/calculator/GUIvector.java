1bbf9303ebf2a85629362e3febe9b6aa10b8f89d
Hldegrd <Sp.Lukas@outlook.com>
2021-05-20 11:20:12 +0200

    hk

--- src/calculator/GUIvector.java
+++ src/calculator/GUIvector.java
@@ -2,35 +2,57 @@ package calculator;
 
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
 
 public class GUIvector extends JFrame {
     private JPanel rootPanel;
-    private JTextField scalaroperator;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
+    private JTextField a1;
+    private JTextField b1;
+    private JTextField c1;
+    private JTextField a2;
+    private JTextField b2;
+    private JTextField c2;
     private JTextField textField7;
     private JTextField textField8;
     private JTextField textField9;
-    private JButton button1;
-
-    public void m_setvisible(boolean visible) {
-        setVisible(visible);
-    }
-
-    public static void main(String [] args) {
-
-        new GUIvector();
-    }
+    private JButton enter;
+    Logic logic;
 
     GUIvector() {
         setLocationRelativeTo(null);
 
         add(rootPanel);
         setSize(400, 400);
+        setVisible(false);
+
+        logic = new Logic();
+
+        enter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double input[] = new double[7];
+                //Designation for Vectorproduct
+                input[0] = 6;
+
+                input[1] = Double.parseDouble(a1.getText());
+                input[2] = Double.parseDouble(b1.getText());
+                input[3] = Double.parseDouble(c1.getText());
+                input[4] = Double.parseDouble(a2.getText());
+                input[5] = Double.parseDouble(b2.getText());
+                input[6] = Double.parseDouble(c2.getText());
+
+                double[] output = new double[3];
+                output = logic.vector_product(input);
+            }
+            });
+
+
+
+
     }
+
 
 }
