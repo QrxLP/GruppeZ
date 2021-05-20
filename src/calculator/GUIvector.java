980562d399bb30ec0bafb34b56c7cea62ca47fe0
Hldegrd <Sp.Lukas@outlook.com>
2021-05-20 10:28:52 +0200

    includes penis

---
+++ src/calculator/GUIvector.java
@@ -0,0 +1,28 @@
+package calculator;
+
+
+import javax.swing.*;
+
+public class GUIvector extends JFrame {
+    private JPanel rootPanel;
+    private JTextField scalaroperator;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JButton button1;
+
+    public void m_setvisible(boolean visible) {
+        setVisible(visible);
+    }
+
+    public static void main(String [] args) {
+
+    }
+
+}
