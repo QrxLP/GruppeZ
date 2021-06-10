e938b62715b4108076e7887c12a9e2aecdae2e1e
BavarianLP <Michael.Zeilmaier@t-online.de>
2021-06-10 11:09:02 +0200

    GUIpythagoras probably fertig

--- src/calculator/Logic.java
+++ src/calculator/Logic.java
@@ -268,9 +268,9 @@ public class Logic {
 
 
         //Initializing the variables which save the values received from the input array.
-        cathete_a = input [1];
-        cathete_b = input [2];
-        hypothenuse_c = input [3];
+        cathete_a = input [0];
+        cathete_b = input [1];
+        hypothenuse_c = input [2];
 
 
         /*
