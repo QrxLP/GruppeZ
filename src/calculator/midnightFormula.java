0cdaf90ca2f3c2221b23890ced3ed06b06283d15
Hldegrd <Sp.Lukas@outlook.com>
2021-06-30 10:05:08 +0200

    exceptions ready

--- src/calculator/midnightFormula.java
+++ src/calculator/midnightFormula.java
@@ -29,34 +29,50 @@ public class midnightFormula extends JFrame{
         enter.setBorderPainted(false);
         enter.setMnemonic(KeyEvent.VK_ENTER);
 
+        readAndcalc();
+
+
+
+
+    }
+
+    private void readAndcalc(){
         enter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double [] input = new double [4];
+                try{
+                    double [] input = new double [4];
                     input[0] = 4;
-                try {
+
                     input[1] = Double.parseDouble(textField1.getText());
                     input[2] = Double.parseDouble(textField2.getText());
                     input[3] = Double.parseDouble(textField3.getText());
-                }
-                catch (NumberFormatException | NullPointerException nfe) {};
 
 
-                double[] output = logic.calc(input);
-                if(output[2] == 1) {
-                    textField4.setText("N/A");
-                    textField5.setText("N/A");
-                    resultPossibleLabel.setVisible(true);
-                } else {
-                    textField4.setText(String.valueOf(output[0]));
-                    textField5.setText(String.valueOf(output[1]));
+
+                    double[] output = logic.calc(input);
+                    if(output[2] == 1) {
+                        textField4.setText("N/A");
+                        textField5.setText("N/A");
+                        resultPossibleLabel.setVisible(true);
+                    } else {
+                        textField4.setText(String.valueOf(output[0]));
+                        textField5.setText(String.valueOf(output[1]));
+                    }
+                } catch (NumberFormatException | NullPointerException ex1) {
+                    ex1.printStackTrace();
+                    System.out.println("No input was found at one or more fields or Input is not " +
+                            "a double!");
+
+                } catch (ArrayIndexOutOfBoundsException ex2) {
+                    ex2.printStackTrace();
+                    System.out.println("Array out of bounds.");
                 }
+
 
 
             }
         });
-
-
     }
 
 
